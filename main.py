efc6414e709eda50ce8ac11ac0be257a5420a4c6
Rodrigo Vittori <119360928+rodrigovittori@users.noreply.github.com>
2025-03-29 13:56:46 -0300

    [M6.L3] Actividad extra: Mini-Clicker
    Utilizamos funciones propias de pgzero para mostrar brevemente las bases de un juego clicker

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 #pgzero
-# [M6.L3] - Actividad #1: "Introducción"
-# Nota: NO enseñar archivos personalizados hasta el final de la clase
+# [M6.L3] - Actividad Extra: Mini-clicker
 
-WIDTH  = 770 # Ancho de la ventana (en px)
-HEIGHT = 450 # Alto de la ventana  (en px)
+WIDTH = 300  # Ancho de la ventana (en px)
+HEIGHT = 300 # Alto de la ventana  (en px)
 
-TITLE = "IMG" # Título de la ventana de juego
-FPS = 30      # Fotogramas (MAX) por segundo
-
-fondo = Actor("picture") # Creamos un Actor con la imagen "picture.jpg"
-
-""" >> Funciones de pgzero << """
+TITLE = "Clicker" # Título de la ventana de juego
+FPS = 30 # Fotogramas por segundo
+contador = 0
 
 def draw():
-    fondo.draw()
+    screen.fill((32, 191, 107))
+    screen.draw.text(str(contador), center=(150, 150), color="white", fontsize = 96)
+    
+def on_mouse_down(button, pos):
+    global contador
+    if button == mouse.LEFT:
+        contador = contador + 1
+        
+    elif button == mouse.RIGHT:
+        contador = contador - 1
