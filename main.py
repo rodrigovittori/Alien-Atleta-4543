a9afbcacbf850f0f3e872de61bc0909ba5d64e39
Rodrigo Vittori <119360928+rodrigovittori@users.noreply.github.com>
2025-03-29 15:01:22 -0300

    [M6.L3] Actividad 7 "Engranaje (adicional)
    Actividad Extra Nº 1: "Engranaje"

--- main.py
+++ main.py
@@ -1,44 +1,37 @@
 #pgzero
-"""
-NOTA 1: El código de este proyecto está publicado en el repo:
-        > https://github.com/rodrigovittori/Alien-Atleta-4543
-    
-NOTA 2: Los assests de este proyecto son del sitio web de Kenney,
-        pueden obtener más modelos en: https://kenney.nl/assets/platformer-pack-redux
-        y revisar la colección completa en: https://kenney.nl/assets/series:Platformer%20Pack  
+# [M6.L3] Actividad 7 "Engranaje (adicional)"
+# Nota: La activdad 6 fue Kahoot! (no lleva código)
 
----------------------------------------------------------------------------------------------------
+WIDTH = 600 # Ancho de la ventana
+HEIGHT = 300 # Altura de la ventana
 
-    [M6.L3] - Actividad #5: "La función update(dt)"
-    Objetivo: Familiarizarnos con la función update() 
-
-    Paso 1) Vamos a crear un nuevo Actor llamado "caja", con la imágen "box"
-    Paso 2) Agregar en draw() el código necesario para que dibuje la caja
-    Paso 3) Vamos a definir nuestro métdodo "update" (para que funcione con pgzero NECESITA su parámetro dt -deltatime-)
-    Paso 4) Agregamos el código necesario para que nuestro personaje se desplace hacia la derecha y la caja hacia la izquierda
-    """
-
-WIDTH = 600 # Ancho de la ventana (en px)
-HEIGHT = 300 # Alto de la ventana (en px)
-
-TITLE = "Juego del Alien Atleta y sus piruetas" # Título para la ventana del juego
+TITLE = "Adicional: Engranaje ⚙️" # Título para la ventana del juego
 FPS = 30 # Número de fotogramas por segundo
 
-""" > Vamos a crear nuestro personaje :D """
-fondo = Actor("background")           # Nuestro fondo NO tiene posición porque queremos que ocupe TODA la pantalla
-personaje = Actor("alien", (50, 240)) # Nuestro personaje SI la tiene, las coordenadas se registran en pos(x, y)
-caja = Actor("box", (WIDTH - 50, 265))
+engranaje = Actor("detail", (WIDTH/2, HEIGHT/2))
+engranaje.sentido = "izquierda"
 
-def draw(): # draw() como su nombre lo indica es el método de pgzero que dibuja objetos en pantalla
-    fondo.draw()
-    personaje.draw()
-    caja.draw()
+def draw():
+    screen.fill(("#8B0000"))
+    engranaje.draw()
 
-def update(dt): # update(dt) es el bucle ppal de nuestro juego, dt significa delta time (tiempo en segundos entre cada frame)
-
-    # Mover al personaje:
-    personaje.x += 5 # mover el personaje 5 px a la derecha en cada frame
+def update(dt):
     
-    # Mover la caja:
-    caja.x -= 5     # mover la caja 5 px a la izquierda en cada frame
-    caja.angle += 5 # roto la caja 5 grados cada frame
+    # Movimiento del engranaje
+    if (engranaje.sentido == "izquierda"):
+        engranaje.angle += 5 # Rotar engranaje
+        if (engranaje.x > int(engranaje.width / 2)):
+            engranaje.x -= 5
+        else:
+            engranaje.sentido = "derecha"
+
+    elif (engranaje.sentido == "derecha"):
+        engranaje.angle -= 5 # Rotar engranaje
+        if (engranaje.x < WIDTH - int(engranaje.width / 2)):
+            engranaje.x += 5
+        else:
+            engranaje.sentido = "izquierda"
+
+
+    
+    
