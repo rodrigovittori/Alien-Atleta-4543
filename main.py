85a7260e1c6649be43cb0054877f7a92a192666f
Rodrigo Vittori <119360928+rodrigovittori@users.noreply.github.com>
2025-03-29 14:06:19 -0300

    [M6.L3] - Actividad 2: "Ventana del Juego"
    Repasamos la creación de una ventana en pgzero

--- main.py
+++ main.py
@@ -1,21 +1,13 @@
 #pgzero
-# [M6.L3] - Actividad Extra: Mini-clicker
+""" [M6.L3] - Actividad 2: "Ventana del Juego"
+    Objetivo: presentar el sistema de pgzero a los alumnos """
 
-WIDTH = 300  # Ancho de la ventana (en px)
+WIDTH = 600  # Ancho de la ventana (en px)
 HEIGHT = 300 # Alto de la ventana  (en px)
 
-TITLE = "Clicker" # Título de la ventana de juego
-FPS = 30 # Fotogramas por segundo
-contador = 0
+FPS = 30
+TITLE = "TÍTULO ÉPICO"
 
 def draw():
-    screen.fill((32, 191, 107))
-    screen.draw.text(str(contador), center=(150, 150), color="white", fontsize = 96)
-    
-def on_mouse_down(button, pos):
-    global contador
-    if button == mouse.LEFT:
-        contador = contador + 1
-        
-    elif button == mouse.RIGHT:
-        contador = contador - 1
+    screen.fill(("#d6af66"))
+    screen.draw.text(TITLE, center=(WIDTH/2, HEIGHT/2), color="white", background="black", fontsize = 48)
