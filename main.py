3f7289ce6ebafdac20dcd9a5a02bab7c973ab745
Rodrigo Vittori <119360928+rodrigovittori@users.noreply.github.com>
2025-03-29 14:11:48 -0300

    [M6.L3] - Actividad 3: "La función draw()"
    Objetivo: Que la clase muestre un mensaje personalizado con el método draw()

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 #pgzero
-""" [M6.L3] - Actividad 2: "Ventana del Juego"
-    Objetivo: presentar el sistema de pgzero a los alumnos """
+""" [M6.L3] - Actividad 3: "La función draw()"
+    Objetivo: Que la clase muestre un mensaje personalizado con el método draw() """
 
-WIDTH = 600  # Ancho de la ventana (en px)
-HEIGHT = 300 # Alto de la ventana  (en px)
+WIDTH = 900 # Ancho de la pantalla (en px)
+HEIGHT = 360 # Alto de la pantalla (en px)
 
-FPS = 30
-TITLE = "TÍTULO ÉPICO"
+TITLE = "TITULO ÉPICO" # sin archivos
+FPS = 30 # Tope/Límite de FPS (cuadros por segundo) a dibujar
+
+string_mensaje = "¡Hola chicos!"
 
 def draw():
-    screen.fill(("#d6af66"))
-    screen.draw.text(TITLE, center=(WIDTH/2, HEIGHT/2), color="white", background="black", fontsize = 48)
+    screen.fill((220, 125, 131))
+    screen.draw.text(string_mensaje, center=(WIDTH/2, HEIGHT/2), color="white", background="black", fontsize = 48)
