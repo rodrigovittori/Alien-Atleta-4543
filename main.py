cfab72fcd062c99bde07cc278de58b53552dd4aa
Rodrigo Vittori <119360928+rodrigovittori@users.noreply.github.com>
2025-04-05 14:09:41 -0300

    [M6.L4] - Actividad Nº 1 "Actualizando la caja"
    Aseguramos que la caja "respawnee" cada vez que se salga de la pantalla

--- main.py
+++ main.py
@@ -9,13 +9,15 @@ NOTA 2: Los assests de este proyecto son del sitio web de Kenney,
 
 ---------------------------------------------------------------------------------------------------
 
-    [M6.L3] - Actividad #9 y #10: Homework/Tareas
-    Objetivo: Agregar el primer obstáculo
+    [M6.L4] - Actividad Nº 1: "Actualizando la caja"
+    Objetivo: Lograr que la caja "respawnee" cada vez que se salga de la pantalla
+    Nota: Los límites para evitar que el PJ se salga de la panmtalla los implementaremos en la Actividad Nº 3 de hoy
 
-    NOTA: Nosotros ya lo resolvimos, nos vemos la clase que viene
-    TAREA: En base a lo que apredieron con el engranaje y los cristales,
-           modifiquen el código para que la caja y nuestro personaje NO puedan salir
-           de la pantalla de juego. """
+    Paso Nº 1) Vamos a poner una condición que detecte CUANDO sale de la pantalla (x < 0)
+    Paso Nº 2) DENTRO de esa condición agregamos la lógica de "respawn"
+                      -> moverla fuera de la panatlla pero del lado derecho
+
+"""
 
 WIDTH = 600 # Ancho de la ventana (en px)
 HEIGHT = 300 # Alto de la ventana (en px)
@@ -35,9 +37,17 @@ def draw(): # draw() como su nombre lo indica es el método de pgzero que dibuja
 
 def update(dt): # update(dt) es el bucle ppal de nuestro juego, dt significa delta time (tiempo en segundos entre cada frame)
 
+    # Podemos traducir "update" como "actualizar", es decir, en ella contendremos el código que produzca cambios en nuestro juego
+
     # Mover al personaje:
     personaje.x += 5 # mover el personaje 5 px a la derecha en cada frame
     
     # Mover la caja:
-    caja.x -= 5     # mover la caja 5 px a la izquierda en cada frame
-    caja.angle += 5 # roto la caja 5 grados cada frame
+    if (caja.x < 0):       # Si la caja salió de la ventana de juego...
+        caja.x += WIDTH    # La llevamos a la otra punta de la pantalla
+    else:
+        # Si todavía no se escapa de la ventana...
+        caja.x -= 5     # mover la caja 5 px a la izquierda en cada frame
+
+    # Rotar la caja
+    caja.angle = (caja.angle % 360) + 5     # roto la caja 5 grados cada frame sin pasarme de 360º
